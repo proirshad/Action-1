f08c38a798ef9a3fdd7f4edc233e76e82157097c
proirshad <64773022+proirshad@users.noreply.github.com>
2025-12-06 14:41:25 +0530

    modify script

--- .github/workflows/ascii.sh
+++ .github/workflows/ascii.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/bin/bash
+sudo apt-get update -y
 sudo apt-get install cowsay -y  
 cowsay -f dragon "Run for cover, i am a dragon....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt 
